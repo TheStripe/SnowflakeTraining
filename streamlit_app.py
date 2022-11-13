391198d1ab41fb1af0c7eeb4a488b05399dd19f5
RobH <robh@live.com>
2022-11-13 15:15:05 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 #streamlit.text("Hello from Snowflake:")
 streamlit.text("Fruit load list:")
-streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
 
 streamlit.title("My parents new healthy dinner!")
 
