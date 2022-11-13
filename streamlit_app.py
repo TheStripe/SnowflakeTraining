618cd1bbad3aa99e5782e60157de47d729e0378b
RobH <robh@live.com>
2022-11-13 09:25:03 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ streamlit.title("My parents new healthy dinner!")
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
+streamlit.multiselect("Pick some fruits", list(my_fruit_list.index))
 
 streamlit.header("Menu")
 streamlit.text("testing...")
