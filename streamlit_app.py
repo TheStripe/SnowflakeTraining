2d56a2c0068b9f49dc73f44fc61c0a9427f4596c
RobH <robh@live.com>
2022-11-13 09:22:57 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 
 import streamlit
+import pandas
 
 streamlit.title("My parents new healthy dinner!")
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
 
 streamlit.header("Menu")
 streamlit.text("testing...")
